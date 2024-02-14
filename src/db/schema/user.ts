061e3634b2ffa59fd5c387e9719bdd976d6603b8
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2024-02-14 10:13:56 +0100

    feat: user tag was added to the schema

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -2,8 +2,9 @@ import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  clerkId: varchar("clerk_id").unique().notNull(),
+  clerkId: varchar("clerk_id").primaryKey().unique().notNull(),
   biography: varchar("biography", { length: 1000 }),
+  tag: varchar("tag").unique().primaryKey().notNull(),
 });
 
 export type InsertUser = InferInsertModel<typeof users>;
