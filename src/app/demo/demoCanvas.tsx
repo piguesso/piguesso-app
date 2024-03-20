b322f188613ae115feaa4db745dca40fca1c1c95
lukasklockenhoff <lukasklockenhoff@icloud.com>
2024-03-20 23:47:30 +0100

    fix: Timer dep

--- src/app/demo/demoCanvas.tsx
+++ src/app/demo/demoCanvas.tsx
@@ -62,7 +62,7 @@ export default function DemoCanvas({
   useEffect(() => {
     getModel();
     new Timer()
-  });
+  }, []);
   const getModel = async () => {
     // loading model
     loadModel().then((model) => {
