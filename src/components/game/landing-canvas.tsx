f93101e2aa12dfa47bb1fc2b6c573697df0ecc0f
lukasklockenhoff <lukasklockenhoff@icloud.com>
2024-03-20 23:42:50 +0100

    fix: broken drawing in landing

--- src/components/game/landing-canvas.tsx
+++ src/components/game/landing-canvas.tsx
@@ -38,19 +38,16 @@ export default function LandingCanvas() {
 
     ctx.beginPath(), ctx.moveTo(points[0].x, points[0].y);
     // draw a bunch of quadratics, using the average of two points as the control point
-    let i;
-    for (i = 1; i < points.length - 2; i++) {
-      let c = (points[i].x + points[i + 1].x) / 2,
-        d = (points[i].y + points[i + 1].y) / 2;
-      ctx.quadraticCurveTo(points[i].x, points[i].y, c, d);
+    for (let i = 1; i < points.length - 1; i++) {
       ctx.quadraticCurveTo(
         points[i].x,
         points[i].y,
         points[i + 1].x,
-        points[i + 1].y,
-      ),
-        ctx.stroke();
+        points[i + 1].y
+      );
+      ctx.stroke();
     }
+    setPoints([points[points.length - 1]]);
   }
 
   return (
